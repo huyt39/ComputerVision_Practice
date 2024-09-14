d7f1e305b45acab3b5594345d3c2319db77a28fe
Huy Tuan <hotuanhuy1203@gmail.com>
2024-09-14 23:05:56 +0700

    bitwise operations

---
+++ OpenCV_CodeCamp/bitwise.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((400, 400), dtype = 'uint8')
+
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+
+cv.imshow('Rectangle', rectangle)
+cv.imshow('Circle', circle)
+
+#bitwise AND: intersecting region
+bitwise_and = cv.bitwise_and(rectangle, circle)
+cv.imshow('AND',bitwise_and)
+
+#OR: non intersecting and intersecting
+bitwise_or = cv.bitwise_or(rectangle, circle)
+cv.imshow('OR', bitwise_or)
+
+#XOR: non intersecting
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('XOR', bitwise_xor)
+
+#NOT: black=> white, white=> black
+bitwise_not = cv.bitwise_not(rectangle)
+cv.imshow('NOT', bitwise_not)
+cv.waitKey(0)
